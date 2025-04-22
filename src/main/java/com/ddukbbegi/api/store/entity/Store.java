4267c0293aa313e90f60cff3f4b22af4f7e52ed8
SeungWoo Ryu <swstar21c@gmail.com>
2025-04-23 00:57:07 +0900

    feat/#2: 동작 확인 완료
    - JPA 설정, 공통 응답 객체, repository 공통 메서드, 예외 처리 등이 정상적으로 동작하는지 간단한게 확인함

--- src/main/java/com/ddukbbegi/api/store/entity/Store.java
+++ src/main/java/com/ddukbbegi/api/store/entity/Store.java
@@ -1,5 +1,6 @@
 package com.ddukbbegi.api.store.entity;
 
+import com.ddukbbegi.api.common.entity.BaseUserEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-public class Store {
+public class Store extends BaseUserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
