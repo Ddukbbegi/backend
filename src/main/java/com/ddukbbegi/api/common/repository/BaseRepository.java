f3fcb8c9fee3b54ab98dd4ddaf9dedfbff766a66
SeungWoo Ryu <swstar21c@gmail.com>
2025-04-23 00:43:05 +0900

    feat/#2: repository 공통 메서드 처리하는 인터페이스 구현
    - 해당 기능을 사용하려면 repository에서 JpaRepository가 아니라 BaseRepository 상속 받으면 됨

---
+++ src/main/java/com/ddukbbegi/api/common/repository/BaseRepository.java
@@ -0,0 +1,22 @@
+package com.ddukbbegi.api.common.repository;
+
+import com.ddukbbegi.common.component.ResultCode;
+import com.ddukbbegi.common.exception.BusinessException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+/**
+ * 여러 도메인의 Repository에서 공통적으로 사용하는 기능을 default 메서드로 구현한 인터페이스
+ * @param <T>   Entity 타입
+ * @param <ID>  Entity ID 타입
+ */
+@NoRepositoryBean
+public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
+
+    default T findByIdOrElseThrow(ID id) {
+        return findById(id).orElseThrow(() ->
+                new BusinessException(ResultCode.NOT_FOUND, "해당 Entity를 찾을 수 없습니다. id = " + id)
+        );
+    }
+
+}
---
+++ src/main/java/com/ddukbbegi/api/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ddukbbegi.api.user.repository;
+
+import com.ddukbbegi.api.common.repository.BaseRepository;
+import com.ddukbbegi.api.user.entity.User;
+
+public interface UserRepository extends BaseRepository<User, Long> {
+}
