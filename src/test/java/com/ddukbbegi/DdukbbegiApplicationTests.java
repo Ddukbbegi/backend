e99124faf7d960501ff9a3be9b9e583f66d7dfe0
YongLeeCode <yonghwan311@gmail.com>
2025-04-22 19:32:19 +0900

    initial commit

---
+++ src/test/java/com/ddukbbegi/DdukbbegiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddukbbegi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdukbbegiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
