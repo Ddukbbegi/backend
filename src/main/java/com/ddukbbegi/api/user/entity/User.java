c7afaf0a1a2b777aada46f62b245ff0a308dc084
sinyoung0403 <tlstjs753@gmail.com>
2025-04-23 15:34:46 +0900

    feat/#16: User Entity 추가
    - SoftDelete 적용
    - userRole Enum 으로 관리

--- src/main/java/com/ddukbbegi/api/user/entity/User.java
+++ src/main/java/com/ddukbbegi/api/user/entity/User.java
@@ -1,16 +1,65 @@
 package com.ddukbbegi.api.user.entity;
 
 import com.ddukbbegi.api.common.entity.BaseTimeEntity;
+import com.ddukbbegi.api.user.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
 
 @Entity
 @Getter
+@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id = ?")
 @Table(name = "users")
+@NoArgsConstructor
 public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    private boolean isDeleted = Boolean.FALSE;
+
+    public User(String email, String password, String name, String phone, UserRole userRole) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.userRole = userRole;
+    }
+
+    public static User of(String email, String password, String name, String phone, UserRole userRole) {
+        return new User(email, password, name, phone, userRole);
+    }
+
+    public void updateEmail(String email) {
+        this.email = email;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+
+    public void updatePhone(String phone) {
+        this.phone = phone;
+    }
 
 }
