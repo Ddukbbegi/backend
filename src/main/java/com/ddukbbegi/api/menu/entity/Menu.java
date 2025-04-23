6c9254b2c43848664179a94371fecbff837a81d9
YongLeeCode <yonghwan311@gmail.com>
2025-04-23 15:01:00 +0900

    feat/#17 메뉴 추가 기능을 위한 메뉴 엔티티 구현

---
+++ src/main/java/com/ddukbbegi/api/menu/entity/Menu.java
@@ -0,0 +1,53 @@
+package com.ddukbbegi.api.menu.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * @packageName    : com.ddukbbegi.api.menu.entity
+ * @fileName       : Menu
+ * @author         : yong
+ * @date           : 4/23/25
+ * @description    :
+ */
+
+@Entity
+@Getter
+public class Menu {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	private int price;
+
+	private String description;
+
+	private String category;
+
+	@Column(name = "is_option")
+	private boolean isOption;
+
+	@Column(name = "store_id")
+	private long storeId;
+
+	@Builder
+	public Menu(String name, int price, String description, String category, boolean isOption, long storeId) {
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.category = category;
+		this.isOption = isOption;
+		this.storeId = storeId;
+	}
+
+	public Menu() {
+
+	}
+}
