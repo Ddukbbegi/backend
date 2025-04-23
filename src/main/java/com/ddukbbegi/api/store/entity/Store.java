2ef4401963d4f18cb8c3bf566b2b80f56c83376f
SeungWoo Ryu <swstar21c@gmail.com>
2025-04-23 17:22:31 +0900

    feat/#23: access = PROTECTED 속성 추가

--- src/main/java/com/ddukbbegi/api/store/entity/Store.java
+++ src/main/java/com/ddukbbegi/api/store/entity/Store.java
@@ -6,6 +6,7 @@ import com.ddukbbegi.api.store.enums.DayOfWeekListConverter;
 import com.ddukbbegi.api.store.enums.StoreStatus;
 import com.ddukbbegi.api.user.entity.User;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -13,8 +14,8 @@ import java.time.LocalTime;
 import java.util.List;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Store extends BaseUserEntity {
 
     @Id
