991d055c20db0eecdd005881443fe7b09f0a0ea9
YongLeeCode <yonghwan311@gmail.com>
2025-04-23 16:56:10 +0900

    refactor: BaseRepository 사용 및 주석 삭제

--- src/main/java/com/ddukbbegi/api/menu/repository/MenuRepository.java
+++ src/main/java/com/ddukbbegi/api/menu/repository/MenuRepository.java
@@ -1,15 +1,7 @@
 package com.ddukbbegi.api.menu.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.ddukbbegi.api.common.repository.BaseRepository;
 import com.ddukbbegi.api.menu.entity.Menu;
 
-/**
- * @packageName    : com.ddukbbegi.api.menu.repository
- * @fileName       : MenuRepository
- * @author         : yong
- * @date           : 4/23/25
- * @description    :
- */
-public interface MenuRepository extends JpaRepository<Menu, Long> {
+public interface MenuRepository extends BaseRepository<Menu, Long> {
 }
