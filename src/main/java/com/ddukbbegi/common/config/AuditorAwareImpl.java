0ae1782ce67deb183b1c2e742983d2107caa477a
sinyoung0403 <tlstjs753@gmail.com>
2025-04-24 19:13:45 +0900

    feat/#50 : AuditorAware 구현
    - AuditorAwareImpl 구현
    - 테스트 전

--- src/main/java/com/ddukbbegi/common/config/AuditorAwareImpl.java
+++ src/main/java/com/ddukbbegi/common/config/AuditorAwareImpl.java
@@ -1,6 +1,9 @@
 package com.ddukbbegi.common.config;
 
+import com.ddukbbegi.common.auth.CustomUserDetails;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 
 import java.util.Optional;
 
@@ -8,6 +11,14 @@ public class AuditorAwareImpl implements AuditorAware<Long> {
 
     @Override
     public Optional<Long> getCurrentAuditor() {
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof CustomUserDetails) {
+            Long userId = ((CustomUserDetails) principal).getUserId();
+            return Optional.of(userId);
+        }
+
         return Optional.empty();
     }
 
