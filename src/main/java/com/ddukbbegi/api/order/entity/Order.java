9f5344ae7dfbb162942f23af84c5eee7a31eb8bf
yeonjookang <96986782+yeonjookang@users.noreply.github.com>
2025-04-24 10:40:05 +0900

    fix: @NotNull 추가

--- src/main/java/com/ddukbbegi/api/order/entity/Order.java
+++ src/main/java/com/ddukbbegi/api/order/entity/Order.java
@@ -4,7 +4,9 @@ import com.ddukbbegi.api.common.entity.BaseUserEntity;
 import com.ddukbbegi.api.order.enums.OrderStatus;
 import com.ddukbbegi.api.store.entity.Store;
 import com.ddukbbegi.api.user.entity.User;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,19 +21,23 @@ public class Order extends BaseUserEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     private Store store;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
 
+    @Nullable
     private String requestComment;
 
     @Builder
-    public Order(User user, Store store, String requestComment) {
+    public Order(User user, Store store, @Nullable String requestComment) {
         this.user = user;
         this.store = store;
         this.orderStatus = OrderStatus.WAITING;
--- src/main/java/com/ddukbbegi/api/order/entity/OrderMenu.java
+++ src/main/java/com/ddukbbegi/api/order/entity/OrderMenu.java
@@ -3,6 +3,7 @@ package com.ddukbbegi.api.order.entity;
 import com.ddukbbegi.api.common.entity.BaseUserEntity;
 import com.ddukbbegi.api.menu.entity.Menu;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,12 +17,15 @@ public class OrderMenu extends BaseUserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     private Order order;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     private Menu menu;
 
+    @NotNull
     private int count;
 
     @Builder
