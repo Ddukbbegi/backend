b8b57c140c4cb317d2547d6a9b6e2a6f7376bf32
yeonjookang <96986782+yeonjookang@users.noreply.github.com>
2025-04-25 00:18:59 +0900

    fix: 페치조인문으로 수정

--- src/main/java/com/ddukbbegi/api/order/repository/OrderMenuRepository.java
+++ src/main/java/com/ddukbbegi/api/order/repository/OrderMenuRepository.java
@@ -2,9 +2,12 @@ package com.ddukbbegi.api.order.repository;
 
 import com.ddukbbegi.api.common.repository.BaseRepository;
 import com.ddukbbegi.api.order.entity.OrderMenu;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OrderMenuRepository extends BaseRepository<OrderMenu, Long> {
-    List<OrderMenu> findByOrderIdInWithMenu(List<Long> orderIds);
+    @Query("SELECT om FROM OrderMenu om JOIN FETCH om.menu WHERE om.order.id IN :orderIds")
+    List<OrderMenu> findByOrderIdInWithMenu(@Param("orderIds") List<Long> orderIds);
 }
