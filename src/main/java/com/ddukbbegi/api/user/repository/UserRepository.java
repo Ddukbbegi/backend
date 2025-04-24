69fe522b523caddfcdf07432bd2ea8b082e02946
sinyoung0403 <tlstjs753@gmail.com>
2025-04-24 19:27:50 +0900

    feat/#38 : UserRepository 기능 추가
    - existsByIdAndIsDeletedTrue 추가

--- src/main/java/com/ddukbbegi/api/user/repository/UserRepository.java
+++ src/main/java/com/ddukbbegi/api/user/repository/UserRepository.java
@@ -3,7 +3,14 @@ package com.ddukbbegi.api.user.repository;
 import com.ddukbbegi.api.common.repository.BaseRepository;
 import com.ddukbbegi.api.user.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends BaseRepository<User, Long> {
 
+
+    boolean existsByIdAndIsDeletedTrue(Long id);
+
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
