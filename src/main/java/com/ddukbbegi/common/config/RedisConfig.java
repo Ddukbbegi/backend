4f8ad4f8e5fa8d67d64cf894a0da4f69f46ec7df
sinyoung0403 <tlstjs753@gmail.com>
2025-04-25 17:39:49 +0900

    feat/#59 : Redis Template 추가
    - Auth, Repository 삭제
    - Redis Template Bean 등록

--- src/main/java/com/ddukbbegi/common/config/RedisConfig.java
+++ src/main/java/com/ddukbbegi/common/config/RedisConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
@@ -27,5 +29,17 @@ public class RedisConfig {
         redisStandaloneConfiguration.setPort(port);
 
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 기본 Serializer 설정 (Key 는 String, Value 는 Object)
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
     }
 }
