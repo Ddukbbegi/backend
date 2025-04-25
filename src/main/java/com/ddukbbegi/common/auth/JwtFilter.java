08c2ca8c7b24e2d1279477a0347a17d0851407b9
sinyoung0403 <tlstjs753@gmail.com>
2025-04-25 17:41:39 +0900

    feat/#59 : 블랙리스트 제약 사항 추가

--- src/main/java/com/ddukbbegi/common/auth/JwtFilter.java
+++ src/main/java/com/ddukbbegi/common/auth/JwtFilter.java
@@ -1,21 +1,27 @@
 package com.ddukbbegi.common.auth;
 
+import com.ddukbbegi.common.component.ResultCode;
+import com.ddukbbegi.common.exception.BusinessException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -29,14 +35,19 @@ public class JwtFilter extends OncePerRequestFilter {
         String bearerJwt = request.getHeader("Authorization");
 
         if (bearerJwt == null) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "JWT 토큰이 필요합니다.");
+            sendErrorResponse(response, ResultCode.TOKEN_INVALID);
             return;
         }
 
         String token = jwtUtil.extractToken(bearerJwt);
 
         if (!jwtUtil.isValidToken(token)) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다.");
+            sendErrorResponse(response, ResultCode.TOKEN_INVALID);
+            return;
+        }
+
+        if (isTokenBlacklisted(token)) {
+            sendErrorResponse(response, ResultCode.TOKEN_BLACKLISTED);
             return;
         }
 
@@ -45,4 +56,35 @@ public class JwtFilter extends OncePerRequestFilter {
 
         filterChain.doFilter(request, response);
     }
+
+    /**
+     * AccessToken 이 Blacklist 에 있는지 검증
+     * @param accessToken
+     * @return boolean
+     */
+    private boolean isTokenBlacklisted(String accessToken) {
+        return redisTemplate.hasKey("blacklist:" + accessToken);
+    }
+
+    /**
+     * Filter Error
+     */
+    public void sendErrorResponse(HttpServletResponse response, ResultCode resultCode) throws IOException {
+        response.setStatus(resultCode.getStatus().value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String jsonErrorResponse = "{"
+                + "\"data\": null,"
+                + "\"result\": {"
+                + "\"status\": " + resultCode.getStatus().value() + ","
+                + "\"code\": \"" + resultCode.getCode() + "\","
+                + "\"message\": \"" + resultCode.getDefaultMessage() + "\","
+                + "\"timestamp\": \"" + LocalDateTime.now() + "\""
+                + "}"
+                + "}";
+
+        response.getWriter().write(jsonErrorResponse);
+    }
+
 }
